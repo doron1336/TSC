feca2615c6f3e5a0cb05f116dba27d619d1f2c40
doron <doronal@liquidity-capital.com>
2024-08-03 20:45:46 +0300

    going through ucr

---
+++ data_for_graphs_ucr.py
@@ -0,0 +1,105 @@
+import os
+import pickle
+
+import numpy as np
+from sklearn.linear_model import RidgeClassifierCV
+
+from comaprisons.comparisons_fisher import fisher_ranking
+from comaprisons.comparisons_mrmr import mrmr_ranking
+from comaprisons.comparisons_relieff import ReliefF
+from diffusionMaps.Diffusion_Maps import dm_ranking, dm_ranking_datafold
+from models.GA import generations
+from utils.JM import JM_flat
+from utils.timit import record_duration
+
+baseDir = "UCRArchive_2018"
+os.chdir(os.path.join(os.getcwd(), baseDir))
+dataset_name = "ACSF1" #example
+
+
+
+@record_duration
+def relieff_ranking(train, target, num_of_features):
+    fs = ReliefF(n_neighbors=110, n_features_to_keep=num_of_features)
+    X_train, selected_features = fs._fit_transform(train, np.asarray(target).astype('int'))
+    return selected_features
+
+
+def random_choosing(num_of_features):
+    array_size = 9996
+    random_indices = np.random.choice(
+        array_size, num_of_features, replace=False)
+    # selected_elements = np.zeros(array_size, dtype=bool)
+    # selected_elements[random_indices] = True
+    return random_indices
+
+
+for num in range(1, 2):
+    directory = os.path.join(os.path.abspath("."), dataset_name, str(num))
+    print("directory ", directory)
+    # Load the Data - miniRocket section
+
+    with open(f"{directory}/{dataset_name}_minirocket_train", 'rb') as file:
+        X_train_transform = pickle.load(file)
+    with open(f"{directory}/{dataset_name}_minirocket_test", 'rb') as file:
+        X_test_transform = pickle.load(file)
+    with open(f"{directory}/{dataset_name}_y_train", 'rb') as file:
+        y_train = pickle.load(file)
+    with open(f"{directory}/{dataset_name}_y_test", 'rb') as file:
+        y_test = pickle.load(file)
+
+    # GA section
+    if os.path.exists(f"{directory}/GA_results"):
+        with open(f"{directory}/GA_results", 'rb') as file:
+            chromo_df_bc, score_bc = pickle.load(file)
+    else:
+        chromo_df_bc, score_bc = generations(X_train_transform, y_train, size=800, n_feat=X_train_transform.shape[1],
+                                             n_parents=640, mutation_rate=0.20, n_gen=2,
+                                             X_train=X_train_transform, X_test=X_test_transform, Y_train=y_train,
+                                             Y_test=y_test)
+    selectedChromo = np.empty(X_train_transform.shape[1])
+
+    for chromo in chromo_df_bc:
+        numOfSelectedFeatures = np.sum(chromo)
+        # if numOfSelectedFeatures > max:
+        # selectedChromo = chromo
+        print("number of features in chromo", numOfSelectedFeatures)
+    new_X_train_transform = X_train_transform[:, chromo_df_bc[1]]
+    JM_flat_data, dataMean = JM_flat(new_X_train_transform, y_train)
+    avg_jm = np.mean(JM_flat_data, axis=1)
+    with open(os.path.join(directory,"avg_jm"), 'wb') as file:
+        pickle.dump(avg_jm, file)
+
+    algo_dict = {0: "fisher", 1: "mrmr", 2: "relief", 3: "dm", 4: "dm_datafold", 5: "random"}
+    predictions_dict = {"fisher": [], "mrmr": [], "relief": [], "dm": [], "dm_datafold": [], "random": []}
+    num_algos = len(algo_dict)
+    for num_features in range(10, 201, 10):
+        fishers_selected = fisher_ranking(new_X_train_transform, y_train, num_features)
+        mrmr_selected = mrmr_ranking(new_X_train_transform, y_train, num_features)
+        relief_selected = relieff_ranking(new_X_train_transform, y_train, num_features)
+        dm_selected, dm_coordinates, labels = dm_ranking(JM_flat_data, num_features, q=100)
+        dm_selected_datafold, dm_coordinates_datafold = dm_ranking_datafold(JM_flat_data, num_features, q=100)
+        random_selected = random_choosing(num_features)
+
+        features_dict = {0: fishers_selected, 1: mrmr_selected,
+                         2: relief_selected, 3: dm_selected, 4: dm_selected_datafold, 5: random_selected}
+
+        dm_data = {"labels": labels, "dm_coordinates": dm_coordinates}
+        with open(os.path.join(directory, f"dm_data_{num_features}_features"), 'wb') as file:
+            pickle.dump(dm_data, file)
+        with open(os.path.join(directory, f"selected_{num_features}_features"), 'wb') as file:
+            pickle.dump(features_dict, file)
+
+        for j in range(num_algos):
+            classifier_selected = RidgeClassifierCV(alphas=np.logspace(-3, 3, 10))
+            classifier_selected.fit(
+                X_train_transform[:, features_dict[j]], y_train)
+            prediction_score = classifier_selected.score(
+                X_test_transform[:, features_dict[j]], y_test)
+            predictions_dict[algo_dict[j]].append(prediction_score)
+
+    for k in range(num_algos):
+        with open(os.path.join(directory,f'{algo_dict[k]}_ga_before'), 'wb') as file:
+            pickle.dump(predictions_dict[algo_dict[k]], file)
+        # with open(f'{algo_dict[k]}_selected', 'wb') as file:
+        #     pickle.dump(algo_dict[k], file)
---
+++ minirocket_ucr.py
@@ -0,0 +1,65 @@
+import os.path
+import pickle
+from typing import Any
+
+import numpy as np
+
+from models.minirocket import fit, transform
+
+baseDir = "UCRArchive_2018"
+# List the contents of the directory with full paths
+datasets = [os.path.join(os.getcwd(), baseDir, item) for item in
+            os.listdir(os.path.join(os.getcwd(), baseDir))]
+
+
+def check_multilabel(y_train: np.ndarray[Any, np.dtype]) -> bool:
+    if np.max(y_train) < 3:
+        return False
+    else:
+        return True
+
+
+for dataset_path in datasets:
+    print(dataset_path)
+    dataset_name = dataset_path.split("/")[-1]
+    if dataset_name == ".DS_Store" or dataset_name == "Missing_value_and_variable_length_datasets_adjusted":
+        continue
+    data_path = os.path.join(f"{dataset_path}_TRAIN.tsv")
+
+    training_data = np.loadtxt(os.path.join(f"{dataset_path}", f"{dataset_name}_TRAIN.tsv"))
+    y_train, x_train = training_data[:, 0].astype(np.int32), training_data[:, 1:]
+    x_train[np.isnan(x_train)] = 0  # fill missing data with 0
+    if (check_multilabel(y_train) and not os.path.isdir(os.path.join(f"{dataset_path}", "1"))
+            and not os.path.isdir(os.path.join(f"{dataset_path}", "2"))
+            and not os.path.isdir(os.path.join(f"{dataset_path}", "3"))):  # check if the dataset is relevant - multilable and if we already saved a result for it
+        test_data = np.loadtxt(os.path.join(f"{dataset_path}", f"{dataset_name}_TEST.tsv"))
+        y_test, x_test = test_data[:, 0].astype(np.int32), test_data[:, 1:]
+        x_test[np.isnan(x_test)] = 0  # fill missing data with 0
+
+        for num in range(1, 4):
+            directory = os.path.join(os.getcwd(), baseDir, dataset_name, str(num))
+            if not os.path.exists(directory):
+                try:
+                    os.makedirs(directory)
+                except:
+                    print("failed to create directory")
+
+            # miniRocket section
+            filename_train = os.path.join(os.path.abspath("."), directory, f"{dataset_name}_minirocket_train")
+            filename_test = os.path.join(os.path.abspath("."), directory, f"{dataset_name}_minirocket_test")
+            filename_y_train = os.path.join(os.path.abspath("."), directory, f"{dataset_name}_y_train")
+            filename_y_test = os.path.join(os.path.abspath("."), directory, f"{dataset_name}_y_test")
+
+            parameters = fit(x_train)
+            X_train_transform = transform(x_train, parameters)
+            X_test_transform = transform(x_test, parameters)
+
+            with open(filename_train, 'wb') as file:
+                pickle.dump(X_train_transform, file)
+            with open(filename_test, 'wb') as file:
+                pickle.dump(X_test_transform, file)
+
+            with open(filename_y_train, 'wb') as file:
+                pickle.dump(y_train, file)
+            with open(filename_y_test, 'wb') as file:
+                pickle.dump(y_test, file)
